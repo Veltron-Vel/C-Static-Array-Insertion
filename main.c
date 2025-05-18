88f4c59af526392754cf0f0215171100d3eff755
Veltron-Vel <77853585+Veltron-Vel@users.noreply.github.com>
2025-05-18 19:11:05 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,index,value;
+    int arr[10] = {1,2,3,4,5,6,7,8,9};
+
+    int length = 9;
+
+    printf("In which index do you want add an element(0-9):\n");
+    scanf("%d",&index);
+    printf("What is the value of the element:\n");
+    scanf("%d",&value);
+
+    for(i=length-1;i>=index;i--){
+        arr[i+1] = arr[i];
+    }
+    arr[index] = value;
+    length++;
+
+    printf("After insertion the array stands:\n");
+    for(i=0;i<length;i++){
+        printf("%d\n",arr[i]);
+    }
+    printf("The length of the array is %d\n",length);
+
+    return 0;
+}
