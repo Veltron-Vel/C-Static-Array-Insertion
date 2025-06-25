b2ffe774b8ae68f10a81d957affe420f6f57d0c9
RaiyanLikesWaffles <77853585+Veltron-Vel@users.noreply.github.com>
2025-06-25 11:18:31 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,70 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+void arrayInsertion(int arr[],int *n,int value,int index);
+void arrayDeletion(int arr[],int *n,int index);
+void printArray(int arr[],int *n);
 
 int main()
 {
-    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
-    int length = 10;
-    int i,index;
 
-    printf("The elements in the array currently are:\n");
-    for(i=0;i<10;i++){
-        printf("%d\n",arr[i]);
+    int arr[10] = {10,20,30,40};
+
+    int length = sizeof(arr) / sizeof(int); // Also can directly say length == 4 so you dont print the extra elements.
+
+    arrayInsertion(arr,&length,50,4);
+    printArray(arr,&length);
+    arrayDeletion(arr,&length,1);
+    printArray(arr,&length);
+
+}
+
+void arrayInsertion(int arr[],int *n,int value,int index)
+{
+    int i;
+
+    if(index<0 || index>*n)
+    {
+        printf("Out of bounds\n");
+        return;
     }
-    printf("Which position do you want to delete:\n"); // Don't create a buffer overflow by using more than 9
-    scanf("%d",&index);
-    for(i=index;i<length-1;i++){
+
+    for(i=(*n)-1;i>=index;i--)
+    {
+        arr[i+1] = arr[i];
+    }
+
+    arr[index] = value;
+    (*n)++; // So you can do the insertion multiple times
+    printf("Suceesfully added value %d at index %d\n",value,index);
+    return;
+}
+
+void arrayDeletion(int arr[],int *n,int index)
+{
+    int i;
+
+    if(index<0 || index>=*n)
+    {
+        printf("Out of bounds\n");
+        return;
+    }
+    for(i=index;i<(*n)-1;i++)
+    {
         arr[i] = arr[i+1];
     }
-    length--;
-    printf("The array after deletion is:\n");
-    for(i=0;i<length;i++){
+    (*n)--;
+
+    printf("Element delted at index %d\n",index);
+}
+
+void printArray(int arr[],int *n)
+{
+    int i;
+
+    printf("Array:\n");
+
+    for(i=0;i<(*n);i++)
+    {
         printf("%d\n",arr[i]);
     }
-
-    return 0;
 }
